1b43be3a01e81e28bbea766c46042ddbd59dcac5
Ismael Jhon Jalbuna <jalbuna.ismael@gmail.com>
2025-07-11 18:34:09 +0800

    Add toolbar option items

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,24 +2,39 @@
   <q-layout view="lHh Lpr lFf">
     <q-header>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-          v-show="false"
-        />
         <q-toolbar-title class="text-subtitle1">Bickert Management Timetracker</q-toolbar-title>
+        <q-btn-dropdown stretch flat dropdown-icon="settings">
+          <q-list>
+            <q-item clickable v-close-popup tabindex="0">
+              <q-item-section avatar>
+                <q-avatar icon="screenshot_monitor" square text-color="grey-7" size="40px" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Take screenshot</q-item-label>
+              </q-item-section>
+            </q-item>
+            <!-- <q-separator /> -->
+            <q-item clickable v-close-popup tabindex="1">
+              <q-item-section avatar>
+                <q-avatar icon="lock" square text-color="grey-7" size="40px" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Access keys</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced />
+            <q-item clickable v-close-popup tabindex="2">
+              <q-item-section avatar>
+                <q-avatar icon="logout" square text-color="grey-7" size="40px" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Sign out</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-      </q-list>
-    </q-drawer>
 
     <q-page-container class="bg-grey-2">
       <router-view />
@@ -27,12 +42,4 @@
   </q-layout>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
-</script>
+<script setup lang="ts"></script>
